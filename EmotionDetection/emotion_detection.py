4fa153a2d11405553011b457d196f047d87db82d
= <=>
2026-08-18 14:57:42 -0400

    Incorporated error handling for status code 400

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -19,3 +19,12 @@ def emotion_detector(text_to_analyze):
             'sadness': values['sadness'],
             'dominant_emotion': dominant_emotion        
         }
+    elif response.status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None        
+        }
