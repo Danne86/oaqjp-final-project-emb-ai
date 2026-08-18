91c223c83bf1a2a2763b2a9b9da964c86e1471ff
= <=>
2026-08-18 15:45:53 -0400

    Added missing index page load

--- server.py
+++ server.py
@@ -2,7 +2,7 @@
     detector to be executed over the Flask channel and deployed on
     localhost:5000.
 '''
-from flask import Flask, request
+from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detector")
@@ -24,6 +24,13 @@ def sent_detector():
         f" 'joy': {result['joy']} and 'sadness': {result['sadness']}. ",
         f"The dominant emotion is {result['dominant_empotion']}."
         )
+
+@app.route("/")
+def render_index_page():
+    ''' This function initiates the rendering of the main application
+        page over the Flask channel
+    '''
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
