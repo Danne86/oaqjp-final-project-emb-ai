2d47d0f32cd5cc99a7253aaf69e9f501b81bfd9a
= <=>
2026-08-18 14:16:22 -0400

    Added formatting

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,7 +1,21 @@
-import requests
+import requests, json
 
 def emotion_detector(text_to_analyze):    
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyze } }
-    requests.post(url, json = myobj, headers=headers)
+    response = requests.post(url, json = myobj, headers=headers)
+
+    if response.status_code == 200:
+        formatted_response = json.loads(response.text)
+        values = formatted_response['emotionPredictions'][0]['emotion']
+        dominant_emotion = max(values, key=values.get)
+        
+        return {
+            'anger': values['anger'],
+            'disgust': values['disgust'],
+            'fear': values['fear'],
+            'joy': values['joy'],
+            'sadness': values['sadness'],
+            'dominant_emotion': dominant_emotion        
+        }
